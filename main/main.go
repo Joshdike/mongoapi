241db25e29d88cb74f0f386b142f090c4af834a4
DJ Phoenix <phoenix@Dikes-MacBook-Air.local>
2024-10-25 03:22:46 +0300

    Initialise Project

---
+++ main/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/Joshdike/mongoapi/controllers"
+	"github.com/julienschmidt/httprouter"
+	"gopkg.in/mgo.v2"
+)
+
+func main() {
+	r := httprouter.New()
+	uc := controllers.NewUserController(getSession())
+
+	r.GET("/user/:id", uc.GetUser)
+	r.GET("/user", uc.GetAllUsers)
+	r.POST("/user", uc.CreateUser)
+	r.PUT("/user/:id", uc.UpdateUser)
+	r.DELETE("/user/:id", uc.DeleteUser)
+
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getSession() *mgo.Session {
+	s, err := mgo.Dial("mongodb://localhost:27017")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return s
+}
